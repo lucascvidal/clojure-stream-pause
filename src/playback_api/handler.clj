f20ea83b78d2b2599d65e27ba1004085c8b96011
Lucas Vidal <lcv55@hotmail.com>
2024-03-17 14:08:35 -0300

    Update get-positions and related functions logic

--- src/playback_api/handler.clj
+++ src/playback_api/handler.clj
@@ -22,23 +22,34 @@
         (-> (response/response "Successful operation.")
           (assoc :status 201))))))
 
+(defn- validate-and-convert-uuid [uuid-str]
+  (when (and uuid-str (not (empty? uuid-str)))
+    (if-let [uuid (utils/string-to-uuid uuid-str)]
+      uuid
+      (throw (Exception. "Invalid UUID")))))
+
 (defn get-positions [request]
-  (let [{:strs [page per_page user_id title_id media_id finished]} (:query-params request)
-        filters (utils/compact {:user_id (utils/string-to-uuid user_id)
-                                :title_id (utils/string-to-uuid title_id)
-                                :media_id (utils/string-to-uuid media_id)
-                                :finished (case finished
-                                            "true" true
-                                            "false" false
-                                            nil nil)})
-        page-int (max 0 (or (try (Integer/parseInt page) (catch Exception _ 0)) 0))
-        per_page-int (max 5 (min 50 (or (try (Integer/parseInt per_page) (catch Exception _ 10)) 10)))
-        positions (position/list-positions filters) ; page-int per_page-int)
-        total (count positions)
-        total-pages (Math/ceil (/ total (float per_page-int)))]
-    (-> (response/response positions)
-        (assoc :status 200)
-        (assoc :headers {"x-page" (str page-int)
-                         "x-per-page" (str per_page-int)
-                         "x-total" (str total)
-                         "x-total-pages" (str (int total-pages))}))))
+  (try
+    (let [{:strs [page per_page user_id title_id media_id finished]} (:query-params request)
+          user-id-uuid (validate-and-convert-uuid user_id)
+          title-id-uuid (validate-and-convert-uuid title_id)
+          media-id-uuid (validate-and-convert-uuid media_id)
+          filters (utils/compact {:user_id user-id-uuid
+                                  :title_id title-id-uuid
+                                  :media_id media-id-uuid
+                                  :finished (case finished "true" true "false" false nil nil)})
+          page-int (max 0 (or (try (Integer/parseInt page) (catch Exception _ 0)) 0))
+          per_page-int (max 1 (min 50 (or (try (Integer/parseInt per_page) (catch Exception _ 10)) 10)))
+          result (position/list-positions filters page-int per_page-int)
+          positions (:positions result)
+          total (:total-count result)
+          total-pages (Math/ceil (/ total (float per_page-int)))]
+      (-> (response/response positions)
+          (assoc :status 200)
+          (assoc :headers {"x-page" (str page-int)
+                           "x-per-page" (str per_page-int)
+                           "x-total" (str total)
+                           "x-total-pages" (str (int total-pages))})))
+    (catch Exception e
+      (-> (response/response (str "Error: " (.getMessage e)))
+          (assoc :status 422)))))
--- src/playback_api/models/position.clj
+++ src/playback_api/models/position.clj
@@ -10,7 +10,21 @@
   (sql/insert! db-spec :positions
                 {:user_id (string-to-uuid user_id), :title_id (string-to-uuid title_id), :media_id (string-to-uuid media_id), :position position, :finished (if finished finished false), :created_at (java.sql.Timestamp. (System/currentTimeMillis))}))
 
-(defn list-positions [filters]
+(defn count-positions [filters]
   (if (empty? filters)
-    (jdbc/execute! db-spec ["SELECT * FROM positions"])
-    (sql/find-by-keys db-spec :positions filters)))
+    (let [query "SELECT COUNT(*) AS count FROM positions"
+          result (sql/query db-spec [query])]
+      (-> result first (get :count)))
+    (let [result (sql/aggregate-by-keys db-spec :positions "count(*)" filters)]
+      (-> result))))
+
+(defn list-positions [filters page per_page]
+  (let [query-options {:order-by [[:id :desc]]
+                       :offset (* page per_page)
+                       :limit per_page}
+        positions (if (empty? filters)
+                    (sql/find-by-keys db-spec :positions :all query-options)
+                    (sql/find-by-keys db-spec :positions filters query-options))
+        total-count (count-positions filters)]
+    {:positions positions
+     :total-count total-count}))
