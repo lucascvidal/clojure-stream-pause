909751417c31ac7051089c134a30f12c56997797
Lucas Vidal <lcv55@hotmail.com>
2024-03-17 21:02:45 -0300

    Update handler_test.clj

--- test/playback_api/handler_test.clj
+++ test/playback_api/handler_test.clj
@@ -1,40 +1,44 @@
 (ns playback-api.handler-test
   (:require [clojure.test :refer :all]
-            [ring.mock.request :as mock]
-            [playback-api.handler :as handler]
-            [cheshire.core :as json]
+            [playback-api.handler :refer :all]
             [playback-api.models.position :as position]))
 
 (deftest test-post-position
-  (let [body-map {:user_id "123e4567-e89b-12d3-a456-426614174000"
-                  :title_id "123e4567-e89b-12d3-a456-426614174000"
-                  :media_id "123e4567-e89b-12d3-a456-426614174000"
-                  :position 10
-                  :finished true}
-        body-json (json/encode body-map)
-        request (mock/request :post "/position" body-json)]
-    (testing "Successful operation."
-      (let [response (handler/post-position (assoc request :body (json/parse-string body-json true)))]
-        (is (= 201 (:status response)))))))
+  (testing "Valid payload"
+    (with-redefs [position/insert-position (fn [_] nil)]
+      (let [request {:body {:user_id "123e4567-e89b-12d3-a456-426614174000"
+                            :title_id "123e4567-e89b-12d3-a456-426614174000"
+                            :media_id "123e4567-e89b-12d3-a456-426614174000"
+                            :position 10
+                            :finished false}}]
+        (let [response (post-position request)]
+          (is (= 201 (:status response)))))))
 
+  (testing "Invalid payload"
+    (with-redefs [position/insert-position (fn [_] nil)]
+      (let [request {:body {:user_id "123"
+                            :title_id "123e4567-e89b-12d3-a456-426614174000"
+                            :media_id "123e4567-e89b-12d3-a456-426614174000"
+                            :position 10
+                            :finished false}}]
+        (let [response (post-position request)]
+          (is (= 422 (:status response)))))))
 
-(deftest test-post-position-with-invalid-uuid
-  (let [body-map {:user_id "123e4567-e89b-12d3-a456-426614174000"
-                  :title_id "123"
-                  :media_id "123e4567-e89b-12d3-a456-426614174000"
-                  :position 10
-                  :finished true}
-        body-json (json/encode body-map)
-        request (mock/request :post "/position" body-json)]
-    (testing "Payload validation failed"
-      (let [response (handler/post-position (assoc request :body (json/parse-string body-json true)))]
-        (is (= 422 (:status response)))))))
+  (testing "Missing body"
+    (with-redefs [position/insert-position (fn [_] nil)]
+      (let [request {:body {}}]
+        (let [response (post-position request)]
+          (is (= 400 (:status response))))))))
 
-(deftest test-post-position-with-empty-body
-  (let [body-map clojure.lang.PersistentHashMap/EMPTY
-        body-json (json/encode body-map)
-        request (mock/request :post "/position" body-json)]
-    (testing "Invalid request payload"
-      (let [response (handler/post-position (assoc request :body (json/parse-string body-json true)))]
-        (is (= 400 (:status response)))))))
+(deftest test-get-positions
+  (testing "Valid UUID for user_id"
+    (with-redefs [position/list-positions (fn [_ _ _] {:positions [] :total-count 0})]
+      (let [request {:query-params {"user_id" "123e4567-e89b-12d3-a456-426614174000"}}]
+        (let [response (get-positions request)]
+          (is (= 200 (:status response)))))))
 
+  (testing "Invalid UUID for user_id"
+    (with-redefs [position/list-positions (fn [_ _ _] {:positions [] :total-count 0})]
+      (let [request {:query-params {"user_id" "invalid-uuid"}}]
+        (let [response (get-positions request)]
+          (is (= 422 (:status response))))))))
